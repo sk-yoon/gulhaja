ebac09f3e32a759508e26f4e6b3080e8cd9c7aa2
SKY <anytimesk@daum.net>
2023-10-27 17:22:40 +0900

    DB 접속 확인

--- db/maria.js
+++ db/maria.js
@@ -3,11 +3,11 @@ const maria = require("mysql");
 
 
 const conn = maria.createConnection({
-  host: "127.0.0.1",
-  post: 3309,
-  user: "gulhaja",
-  password: "1234",
-  database: "display",
+  host : 'svc.sel5.cloudtype.app',
+    port:32191,
+    user:'gulhaja',
+    password:'1234',
+    database:'display'
 });
 
 module.exports = conn;
