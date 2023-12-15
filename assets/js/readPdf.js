a58b67144ffc03141ca6d34ba9b926d23fb34df9
SKY <anytimesk@daum.net>
2023-12-15 17:44:09 +0900

    code 수정

--- assets/js/readPdf.js
+++ assets/js/readPdf.js
@@ -1,14 +1,14 @@
 const pdfjsLib = window["pdfjs-dist/build/pdf"];
 
-pdfjsLib.GlobalWorkerOptions.workerSrc =
-  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.11.338/pdf.worker.min.js";
+pdfjsLib.GlobalWorkerOptions.workerSrc = "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.11.338/pdf.worker.min.js";
 
 let coverScale = 0.4; // Adjust as needed
 let pageScale = 1; // Adjust as needed
 let intervalInSeconds = 1; // Interval to switch PDFs in seconds
 
 let currentPdfIndex = 0;
-let watchDog = 0
+let watchDog = 0;
+var currentPdf;
 
 async function renderCoverPage(pdfDoc, canvas) {
   let firstPage = await pdfDoc.getPage(1);
@@ -46,7 +46,7 @@ async function renderPdfPage(pdfDoc, canvas, pageNumber) {
   });
 }
 
-async function switchPdf(coverPdf) {
+async function switchPdf(data) {
   let canvas = document.getElementById("pdf-cover");
 
   $("#writeId").text(data[currentPdfIndex].WRITER_NO);
@@ -54,8 +54,8 @@ async function switchPdf(coverPdf) {
   $("#writer").text(data[currentPdfIndex].NAME);
   $("#affilication").text(data[currentPdfIndex].AFFILIATION);
   $("#grade").text(data[currentPdfIndex].GRADE);
-
-  await renderCoverPage(coverPdf, canvas);
+  
+  await renderCoverPage(currentPdf, canvas).promise;
 }
 
 let currentPage = 0;
@@ -63,13 +63,11 @@ async function switchPdfPage(data) {
   let l_pdfUrl = document.location.origin + "/uploads/" + data[currentPdfIndex].FILENAME;
   let l_even_canvas = document.getElementById("pdf-page");
 
-  let pdfDoc = await pdfjsLib.getDocument(l_pdfUrl).promise;
-
-  if (currentPage >= pdfDoc.numPages) {
+  if (currentPage >= currentPdf.numPages) {
     if ((currentPdfIndex + 1) != data.length){
       currentPdfIndex = (currentPdfIndex + 1) % data.length; // Move to the next PDF file
     } else {
-      if (watchDog < 10) {
+      if (watchDog < 1) {
         currentPdfIndex = (currentPdfIndex + 1) % data.length; // Move to the next PDF file
         watchDog += 1
       } else {
@@ -77,22 +75,24 @@ async function switchPdfPage(data) {
       }
     }
     l_pdfUrl = document.location.origin + "/uploads/" + data[currentPdfIndex].FILENAME;
-    pdfDoc = await pdfjsLib.getDocument(l_pdfUrl).promise;
-    await switchPdf(pdfDoc);
+  
+    currentPdf = await pdfjsLib.getDocument(l_pdfUrl).promise;
+    await switchPdf(data);
 
     currentPage = 1;
   } else {
     currentPage++;
   }
 
-  renderPdfPage(pdfDoc, l_even_canvas, currentPage); // Display the second page (change '2' to display other pages if needed)
+  renderPdfPage(currentPdf, l_even_canvas, currentPage); // Display the second page (change '2' to display other pages if needed)
 }
 
 async function showPDF(data) {
-  let pdfUrl = document.location.origin + "/uploads/" + data[currentPdfIndex].FILENAME;
-  let coverPdf = await pdfjsLib.getDocument(pdfUrl).promise;
+  let pdfUrl = document.location.origin + "/uploads/" + data[0].FILENAME;
+  currentPdf = await pdfjsLib.getDocument(pdfUrl).promise;
+  data[0].PDF = currentPdf;
 
-  switchPdf(coverPdf);
+  switchPdf(data);
   switchPdfPage(data);
 
   setInterval(async () => {
